67129f119edeb55b1b18ff8df5ce0fab5369f959
YuvrajSin2003 <yuvrajraghuvanshisingh@gmail.com>
2025-09-05 11:21:38 +0530

    exploring get , post delete

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use("/help" , (req,res) => {
-    res.send("help me");
+app.get("/user", (req, res) => {
+  res.send("Hello form the server");
 });
-app.use("/" , (req,res) => {
+app.delete("/user", (req, res) => {
+  console.log("data is deleted");
+  res.send("data is deleted");
+});
+app.post("/user", (req, res) => {
+  console.log("data is saved");
+  res.send("data is saved");
+});
+app.listen(3000, () => {
+  app.use("/", (req, res) => {
     res.send("Hello i am the fucking root");
+  });
+  console.log("server is sccessfull listing on port 3000");
 });
-app.use("/test" , (req,res) => {
-    res.send("server is testing");
-});
-app.listen(3000 , () => {
-    console.log("server is sccessfull listing on port 3000");
-})
