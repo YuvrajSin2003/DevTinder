e582cbd83e08bddbefdd8ded398dabc652868140
YuvrajSin2003 <yuvrajraghuvanshisingh@gmail.com>
2025-09-10 08:28:33 +0530

    complete api

--- src/app.js
+++ src/app.js
@@ -12,13 +12,49 @@ app.post("/signup",async (req,res) => {
   const user = new User(req.body);
 try{
    await user.save();
- res.send("USer adding successfully")
+ res.send("User adding successfully")
 } catch(err){
   res.status(400).send("error saving");
 }
 });
 
+//get user by email
+app.get("/user" , async(req,res) => {
+  const userEmail = req.body.emailId;
+  try{
+    const users = await User.find({emailId : userEmail})
+    if(users.length === 0){
+      return res.status(404).send("user not found");
+    }else{
+      res.send(users); 
+    }
+  }catch(err){
+    res.send(400).send("something went wrong");
+  }
+})
 
+//to delete user
+app.delete("/user" , async(req,res) => {
+  const userId = req.body.userId;
+  try{
+    const user = await User.findByIdAndDelete({_id:userId});
+    res.send("User deleted");
+  }catch(err){
+    res.status(400).send("spmenthing went wrong");
+  }
+})
+
+// update user
+app.patch("/user" , async(req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try{
+    await User.findByIdAndUpdate(userId ,data);
+    res.send("user updated")
+  }catch(err){
+    res.status(400).send("something went wrong");
+  }
+})
 
 connectDB()
   .then(() => {
